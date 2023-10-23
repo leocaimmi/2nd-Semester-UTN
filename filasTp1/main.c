bc2ea5d796fe7f7cc0f6a2e5e3c3f41b53580981
leocaimmi <leonardocaimmi@icloud.com>
2023-10-23 00:17:52 -0300

    1er tp filas,cambios

--- filasTp1/main.c
+++ filasTp1/main.c
@@ -4,7 +4,7 @@
 int main()
 {
     srand(time(NULL));
-    char archivo[25] = "clientesBorrados";
+    char archivo[25] = "clientesBorrados";///archivo clientes borrados(esta en WB)
     menu(archivo);
     return 0;
 }
